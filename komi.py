6393684877cec4f373e37e28d9237ceea2c2636f
Nick <nickkossolapov@gmail.com>
2019-05-18 20:33:49 +0200

    Update dir name options

--- komi.py
+++ komi.py
@@ -8,7 +8,8 @@ from selenium.webdriver.firefox.options import Options
 base_url = "https://bato.to"
 
 
-def save_image(div, chapter):
+def save_image(div, title, folder):
+    chapter = title.split("-")[0].strip().split(".")[-1].zfill(3)
     img_url = div.find("img").attrs["src"]
     img_page_full = div.find("span", {"class": "page-num"}).contents[0]
     img_page = img_page_full.split('/')[0].strip().zfill(3)
@@ -18,13 +19,13 @@ def save_image(div, chapter):
 
     image_request = requests.get(img_url, headers={'User-Agent' : "Magic Browser"})
     if image_request.status_code == 200:
-        filename = f"./komi/Ch.{chapter}/{chapter}{img_page}.jpg"
+        filename = f"./{folder}/Ch.{title}/{chapter}{img_page}.jpg"
         os.makedirs(os.path.dirname(filename), exist_ok=True)
         with open(filename, 'wb') as f:
             f.write(image_request.content)
 
 
-def download_chapter(path, driver):
+def download_chapter(path, driver, folder):
     url = f"{base_url}{path}"
     print(f"Downloading {url}")
     driver.get(url)
@@ -32,25 +33,25 @@ def download_chapter(path, driver):
     soup = BeautifulSoup(driver.page_source, 'html.parser')
     title = soup.find("option", selected=True).contents[0].replace(":", "-")
     print(f"Title: {title}")
-    chapter = title.split("-")[0].strip().split(".")[-1].zfill(3)
     viewer = soup.find("div", {"id": "viewer"})
     divs = viewer.find_all("div", {"class": "item"})
 
     for div in divs:
-        save_image(div, chapter)
+        save_image(div, title, folder)
 
     next_path_div = soup.find("div", {"class": "nav-next"})
     if next_path_div:
         next_path = next_path_div.find("a").attrs["href"]
-        download_chapter(next_path, driver)
+        download_chapter(next_path, driver, dir)
 
 
 if __name__ == "__main__":
     # print("Enter path: ")
     # input_path = input()
     input_path = "/chapter/634237"
+    input_folder = 'komi'
 
     options = Options()
     options.headless = True
     with webdriver.Firefox(options=options, executable_path=r'./geckodriver/geckodriver.exe') as gecko_driver:
-        download_chapter(input_path, gecko_driver)
+        download_chapter(input_path, gecko_driver, input_folder)
